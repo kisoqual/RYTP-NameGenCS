e4973f0124b72f6e342865c0c679e7b5fbf2efa5
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-13 19:36:51 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,4 +1,4 @@
-using System; // говорят по хорошему лучше его юзать
+using System; // говорят по хорошему лучше его юзать, но с C#9 необязателен
 class Lmao // класс пишем с большой буквы
 {
     static void lulz(string i="")
@@ -7,7 +7,7 @@ class Lmao // класс пишем с большой буквы
         string j = Console.ReadLine();
         Console.WriteLine(j + i);
 
-        //// только c C# 9 (.NET 5)
+        //// только c C#9 (.NET 5)
         //System.Console.Write("какой ты RYTP хочеш? ");
         //string j = System.Console.ReadLine();
         //System.Console.WriteLine(j+i);
