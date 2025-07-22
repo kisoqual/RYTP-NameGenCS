926a7e7fec0af06638e1436e20517bae92f907fa
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-22 07:14:14 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,30 +2,33 @@ using System; // говорят по хорошему лучше его юзат
 //using static System.Console; //доступно только с C# 10 (.NET 6 (core))
 //using System.Console; - так не работает
 
-class Lmao // класс пишем с большой буквы
+namespace RYTP_NameGenCS
 {
-    static void lulz(string i)
+    class Lmao // класс пишем с большой буквы
     {
-        Console.Write("какой ты RYTP хочеш? ");
-        string j = Console.ReadLine();
-        Console.WriteLine(j + i);
-
-        // только с C# 10 (.NET 6 (core))
-        /*
-        Write("какой ты RYTP хочеш? ");
-        string j = Console.ReadLine();
-        WriteLine(j + i);
-        */
-        
-        // только c C#9 (.NET 5 (core))
-        /*
-        System.Console.Write("какой ты RYTP хочеш? ");
-        string j = System.Console.ReadLine();
-        System.Console.WriteLine(j+i);
-        */
-    }
-    static void Main()
-    {
-        lulz(" RYTP");
+        static void lulz(string i)
+        {
+            Console.Write("какой ты RYTP хочеш? ");
+            string j = Console.ReadLine();
+            Console.WriteLine(j + i);
+    
+            // только с C# 10 (.NET 6 (core))
+            /*
+            Write("какой ты RYTP хочеш? ");
+            string j = Console.ReadLine();
+            WriteLine(j + i);
+            */
+            
+            // только c C#9 (.NET 5 (core))
+            /*
+            System.Console.Write("какой ты RYTP хочеш? ");
+            string j = System.Console.ReadLine();
+            System.Console.WriteLine(j+i);
+            */
+        }
+        static void Main()
+        {
+            lulz(" RYTP");
+        }
     }
 }
